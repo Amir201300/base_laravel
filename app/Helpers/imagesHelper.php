33b33a9d0a8d509c2074c3dfaa770e847186ef5c
Amir <ahmedamirr56@gmail.com>
2020-11-10 17:18:36 +0200

    Api Auth

---
+++ app/Helpers/imagesHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @param string $folder
+ * @param $image
+ * @return string
+ */
+function getImageUrl(string $folder, $image)
+{
+    if ($image)
+        return get_baseUrl() . '/images/' . $folder . '/' . $image;
+    return get_baseUrl() . '/images/1.png';
+}
+
+/**
+ * @param $folder
+ * @param $file
+ * @return string
+ */
+function saveImage($folder, $file)
+{
+    $image = $file;
+    $input['image'] = mt_rand() . time() . '.' . $image->getClientOriginalExtension();
+    $dist = public_path('/images/' . $folder . '/');
+    $image->move($dist, $input['image']);
+    return $input['image'];
+}
+
+/**
+ * @param $folder
+ * @param $file
+ * @return int
+ */
+function deleteFile($folder,$file)
+{
+    $file = public_path('/images/'.$folder.'/'.$file);
+    if(file_exists($file))
+    {
+        File::delete($file);
+    }
+    return 1;
+}
